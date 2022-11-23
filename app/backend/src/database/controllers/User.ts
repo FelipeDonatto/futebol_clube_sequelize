2e8a22e22b247f706cba197fe2ecd4aff3ca3593
FelipeDonatto <fe.donatto@outlook.com>
2022-11-23 18:41:51 -0300

    validações feitas (?)

--- app/backend/src/database/controllers/User.ts
+++ app/backend/src/database/controllers/User.ts
@@ -5,18 +5,20 @@ import { findOne } from '../services/User';
 
 export async function userLogin(req: Request, res: Response) {
   const { email, password } = req.body;
+
   if (!email || !password) {
-    return res
-      .status(400)
-      .json({ message: 'Some required fields are missing' });
+    return res.status(400).json({ message: 'All fields must be filled' });
   }
+
   const user = await findOne(email);
   const userData = user?.dataValues;
   const checkPassword = await compare(password, userData.password);
+
   if (user === null || !checkPassword) {
     return res.status(400).json({ message: 'Invalid fields' });
   }
-  res.status(200).json({
+
+  return res.status(200).json({
     token: JWT.sign({ data: { userData } }, process.env.JWT_SECRET as string, {
       expiresIn: '24h',
     }),
